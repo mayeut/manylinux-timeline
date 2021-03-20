a8d036bf6e6de19aab2095aceb62aa73a0255666
mayeut <mayeut@users.noreply.github.com>
2021-03-20 19:19:33 +0100

    Add glibc versions for consumer statistics

--- update_consumer_stats.py
+++ update_consumer_stats.py
@@ -57,6 +57,13 @@ def update(path: Path, start: datetime, end: datetime):
         date_ = date_ + timedelta(days=1)
 
     df = pd.concat(dataframes)
+    df_glibc = (
+        df[["day", "glibc_version", "num_downloads"]]
+        .groupby(["day", "glibc_version"])
+        .aggregate(np.sum)
+    )
+    df_glibc_all = df_glibc.groupby(["day"]).aggregate(np.sum)
+    df_glibc_stats = df_glibc / df_glibc_all
     pip_version = df["pip_version"].str.split(".", n=2, expand=True)
     df["pip_major"] = pd.to_numeric(pip_version[0])
     df["pip_minor"] = pd.to_numeric(pip_version[1])
@@ -160,11 +167,47 @@ def update(path: Path, start: datetime, end: datetime):
         "last_update": datetime.now(timezone.utc).strftime("%A, %d %B %Y, %H:%M:%S %Z"),
         "index": list([d.date().isoformat() for d in df_python_all.index]),
     }
-    versions = ["2.7", "3.5", "3.6", "3.7", "3.8", "3.9"]
+
+    # combine some versions to remove some of the less used ones
+    # but still accounting for the smaller one
+    glibc_versions = [
+        ("2.5", "2.6"),
+        ("2.7", "2.8"),
+        ("2.9", "2.10", "2.11"),
+        ("2.12", "2.13", "2.14", "2.15", "2.16"),
+        ("2.17", "2.18"),
+        ("2.19", "2.20", "2.21", "2.22"),
+        ("2.23",),
+        ("2.24", "2.25"),
+        ("2.26",),
+        ("2.27",),
+        ("2.28", "2.29", "2.30"),
+    ] + list([(f"2.{minor}",) for minor in range(31, 34)])
+    glibc_versions = glibc_versions[::-1]
+    glibc_version = dict[str, Union[list[str], list[float]]]()
+    glibc_version["keys"] = list([v[0] for v in glibc_versions])
+    for versions in glibc_versions:
+        stats = []
+        for day in out["index"]:
+            value = 0.0
+            for version in versions:
+                try:
+                    value += float(
+                        df_glibc_stats.loc[
+                            (pd.to_datetime(day), version), "num_downloads"
+                        ]
+                    )
+                except KeyError:
+                    pass
+            stats.append(float(f"{100.0 * value:.2f}"))
+        glibc_version[versions[0]] = stats
+    out["glibc_version"] = glibc_version
+
+    python_versions = ["2.7", "3.5", "3.6", "3.7", "3.8", "3.9"]
     python_version = dict[str, Union[list[str], list[float]]]()
     policy_readiness = dict[str, dict[str, Union[list[str], list[float]]]]()
-    python_version["keys"] = versions
-    for version in versions:
+    python_version["keys"] = python_versions
+    for version in python_versions:
         stats = []
         for day in out["index"]:
             try:
@@ -196,9 +239,10 @@ def update(path: Path, start: datetime, end: datetime):
                     )
                 except KeyError:
                     value = 0.0
-                stats.append(float(f"{100.0 * value:.1f}"))
+                stats.append(float(f"{100.0 * value:.2f}"))
             policy_readiness_ver[policy] = stats
     out["python_version"] = python_version
     out["policy_readiness"] = policy_readiness
+
     with open(utils.CONSUMER_DATA_PATH, "w") as f:
         json.dump(out, f, separators=(",", ":"))
