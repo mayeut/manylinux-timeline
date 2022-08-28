aa7d366a622c2d8d3f6c2355d786ffc7cd6bc37c
mayeut <mayeut@users.noreply.github.com>
2022-08-28 10:11:20 +0200

    Don't show pp27 & pp36, add pp39

--- update_stats.py
+++ update_stats.py
@@ -14,10 +14,10 @@ _LOGGER = logging.getLogger(__name__)
 POLICIES = ("ml1", "ml2010", "ml2014", "ml_2_24", "ml_2_27", "ml_2_28", "ml_2_31")
 ARCHITECTURES = ("x86_64", "i686", "aarch64", "ppc64le", "s390x", "armv7l")
 # python implementations are a bit more complicated...
-IMPL_X2 = ("cp27", "pp27")
+IMPL_X2 = ("cp27",)
 IMPL_CP3_FIRST = 5
 IMPL_CP3_LAST = 11
-IMPL_PP3 = tuple(f"pp3{i}" for i in range(6, 8 + 1))
+IMPL_PP3 = tuple(f"pp3{i}" for i in range(7, 9 + 1))
 # that's what is ultimately displayed
 IMPLEMENTATIONS = tuple(
     itertools.chain(
