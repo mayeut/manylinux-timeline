b4cbb60cbd62fd805d2a1b42372327ef8d12e08b
mayeut <mayeut@users.noreply.github.com>
2022-10-16 13:22:19 +0200

    use a thread pool to build package cache

--- update_cache.py
+++ update_cache.py
@@ -1,7 +1,10 @@
 import json
 import logging
 import urllib.parse
+from dataclasses import dataclass
 from datetime import date, datetime
+from enum import Enum
+from multiprocessing.pool import ThreadPool
 from pathlib import Path
 from shutil import move
 
@@ -12,11 +15,25 @@ import utils
 _LOGGER = logging.getLogger(__name__)
 
 
+class Status(Enum):
+    PROCESSED = 1
+    REMOVED = 2
+    MOVED = 3
+    ERROR = 4
+
+
+@dataclass
+class PackageStatus:
+    name: str
+    status: Status
+
+
 def _build_url(package: str) -> str:
     return f"https://pypi.org/pypi/{package}/json"
 
 
-def _package_update(package: str) -> str | None:
+def _package_update(package: str, handle_moved: bool = False) -> PackageStatus:
+    _LOGGER.info(f'"{package}": begin update')
     headers = {"User-Agent": utils.USER_AGENT}
     package_new_name = package
     cache_file = utils.get_release_cache_path(package)
@@ -30,12 +47,14 @@ def _package_update(package: str) -> str | None:
     except requests.exceptions.HTTPError as e:
         if response.status_code == 404:
             _LOGGER.warning(f'"{package}": not available on PyPI anymore')
-            return None
+            return PackageStatus(package, Status.REMOVED)
         else:
             _LOGGER.error(f'"{package}": error "{e}" when retrieving info')
-            return package_new_name  # keep package anyway
+            return PackageStatus(package, Status.ERROR)
     for response_prev in response.history[::-1]:
         if response_prev.status_code == 301:
+            if not handle_moved:
+                return PackageStatus(package, Status.MOVED)
             new_location = response_prev.headers["location"]
             uri = urllib.parse.urlparse(new_location)
             package_new_name = Path(uri.path).parent.name
@@ -50,7 +69,7 @@ def _package_update(package: str) -> str | None:
         if package_new_name != package:
             cache_file_new = utils.get_release_cache_path(package_new_name)
             move(cache_file, cache_file_new)
-        return package_new_name
+        return PackageStatus(package_new_name, Status.PROCESSED)
     elif package_new_name != package:
         cache_file = utils.get_release_cache_path(package_new_name)
 
@@ -86,7 +105,7 @@ def _package_update(package: str) -> str | None:
             info["releases"].pop(release)
     with open(cache_file, "w") as f:
         json.dump(info, f)
-    return package_new_name
+    return PackageStatus(package_new_name, Status.PROCESSED)
 
 
 def update(packages: list[str]) -> list[str]:
@@ -94,14 +113,24 @@ def update(packages: list[str]) -> list[str]:
     packages_set = set(packages)
     to_remove = set()
     to_add = set()
-    for package in sorted(packages_set):
-        _LOGGER.info(f'"{package}": begin update')
-        new_package = _package_update(package)
-        if new_package is None:
+    to_reprocess = set()
+
+    with ThreadPool(32) as pool:
+        for package_status in pool.imap(_package_update, sorted(packages), chunksize=1):
+            if package_status.status == Status.PROCESSED:
+                pass
+            elif package_status.status == Status.REMOVED:
+                to_remove.add(package_status.name)
+            else:
+                assert package_status.status in {Status.MOVED, Status.ERROR}
+                to_reprocess.add(package_status.name)
+
+    for package in sorted(to_reprocess):
+        package_status = _package_update(package, handle_moved=True)
+        if package_status.status == Status.REMOVED:
+            to_remove.add(package_status.name)
+        elif package_status.name != package:
             to_remove.add(package)
-        elif new_package != package:
-            to_remove.add(package)
-            to_add.add(new_package)
-        _LOGGER.debug(f'"{package}": end update')
+            to_add.add(package_status.name)
 
     return list(sorted((packages_set - to_remove) | to_add))
